71e874bd13fc2675877657bd1c50623682196a61
ajajw <111954733+ajajw@users.noreply.github.com>
2023-01-06 17:30:17 -0500

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
-API_ID = ''
-API_HASH = ''
-SESSION = ''
-DESTINATION = ''  # получатель
-CHATS = ('chat_name',)  # кортеж из телеграм чатов, которые мы будем парсить
-KEY_WORDS = ('python',) # ключевые слова которые должны быть в сообщении
+API_ID = '11849455'
+API_HASH = '0956032efc5694f60156fe65f9c19764'
+SESSION = '1AZWarzUBu3llHWYfeNQywoKh8dB2N6hmFqesjNk2VavYU_HlO8Z06URLkUTJhjAGXprOzjqQIKH-MJ78ssOjLW4HyCjVvd4I5gfmH38u413E2fzSItUVXLnGaruIdbGZaF1oxAcSNp8vzl8PQeZADRHb1yNiUy9RKBwWfcqrD6rjZz4abM9NrIrtWwROt7EBwWCJDcujzvAc5Ng801D73gqBxBZMq3mEo71iF4vseEaBZX1BSVZQ3NhWt0WFJgFgGQ1w_v1R80R4D0kAz0p6jFvWQSqr0wrwyb16JZegKzyFSLXNouMuOJ26Zy_UYoP1I0yC9EgDfuOzgx-UvbXzxmp-2Jlswac='
+DESTINATION = '@alvkslspqpqpqoqqq'  # получатель
+CHATS = ('@RemChatChk')  # кортеж из телеграм чатов, которые мы будем парсить
+KEY_WORDS = ('Approved',) # ключевые слова которые должны быть в сообщении
